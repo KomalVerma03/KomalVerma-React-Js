15ab4d8e21600ab5ff234475812e0361e4bb273c
KomalVerma03 <134855903+KomalVerma03@users.noreply.github.com>
2023-10-20 17:39:43 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function Intro() {
+    return(
+        <div className="Intro">
+            <h1>Komal Verma</h1>
+            <p>A 2nd Year Student of Delhi University</p><p>
+           Pursuing the degree of Computer Science hons from Indraprastha College for Women</p>
+           <p><h2>7 day Bootcamp</h2></p>
+           <p><h1>DAY 1</h1></p>
+            
+        </div>
+    );
+} 
+export default Intro;
